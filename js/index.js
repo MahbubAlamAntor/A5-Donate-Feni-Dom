078fca0c2d6a83f385c6071a43750ec346163b20
Mahbub8340 <mdmahbubalam573125@gmail.com>
2024-09-23 19:04:10 +0600

    first and section input section complete with dom

--- js/index.js
+++ js/index.js
@@ -12,22 +12,33 @@ document.getElementById('history-btn-show').addEventListener('click', function(e
     donateSection.classList.add('hidden')
 
     donationBtnShow.classList.remove('bg-primary')
+    donationBtnShow.classList.add('border', 'border-borderColor')
+})
+document.getElementById('donation-btn-show').addEventListener('click', function(e){
+    historySection.classList.add('hidden');
+    historyBtnShow.classList.remove('bg-primary');
+    historyBtnShow.classList.add('border', 'text-fontPrimary');
+
+    donateSection.classList.remove('hidden')
+
+    donationBtnShow.classList.add('bg-primary');
+    donationBtnShow.classList.remove('border', 'border-borderColor');
 })
 
-
-
+// card 1
 document.getElementById('donate-now-btn').addEventListener('click', function(e){
     // incresse amount
-    const donateAmount = parseFloat(document.getElementById('donate-amount').value);
+    const donateTitle = getInnerText('donate-title');
+    const donateAmount = getInnerValue('donate-amount');
     const accountBalance = allBalance('account-balance');
     const myAvailableBalance = allBalance('my-available-balance');
 
     if(isNaN(donateAmount)){
-        alert('not valid number'); 
-        return;
+        return alert('not valid number'); 
     }else if(myAvailableBalance < donateAmount){
-        alert('influence balance');
-        return;
+        return alert('influence balance');
+    }else if(donateAmount < 0){
+        return alert('Your Amount Not Valid')
     }
     else{
         const newTotalBalance = donateAmount + accountBalance;
@@ -39,4 +50,64 @@ document.getElementById('donate-now-btn').addEventListener('click', function(e){
 
     const newMyAvailableBalance = myAvailableBalance - donateAmount;
     document.getElementById('my-available-balance').innerText = newMyAvailableBalance;
+
+
+    // append history
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="border border-borderColor2 p-5 rounded-xl">
+       <h4 class="font-bold pb-3">${donateAmount} Taka is Donated ${donateTitle}</h4>
+       <span class='text-gray-500'>Date: ${new Date()}<span>
+    </div>
+    `
+    // document.getElementById('history-section').appendChild(div);
+   historySection.insertBefore(div, historySection.firstChild);
 })
+
+
+// card no 2
+
+document.getElementById('donate-now-btn1').addEventListener('click', function(e){
+    // incresse amount
+    const donateTitle = getInnerText('donate-title');
+    const donateAmount = getInnerValue('donate-amount1');
+    const accountBalance = allBalance('account-balance1');
+    console.log(accountBalance.value)
+    const myAvailableBalance = allBalance('my-available-balance');
+    console.log(accountBalance)
+
+    if(isNaN(donateAmount)){
+        return alert('not valid number'); 
+    }else if(myAvailableBalance < donateAmount){
+        return alert('influence balance');
+    }else if(donateAmount < 0){
+        return alert('Your Amount Not Valid')
+    }
+    else{
+        const newTotalBalance = donateAmount + accountBalance;
+        console.log(newTotalBalance)
+        document.getElementById('account-balance1').innerText = newTotalBalance;
+        document.getElementById('my_modal_51').showModal();
+    }
+
+    // decrese amount
+
+    const newMyAvailableBalance = myAvailableBalance - donateAmount;
+    document.getElementById('my-available-balance').innerText = newMyAvailableBalance;
+
+
+    // append history
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="border border-borderColor2 p-5 rounded-xl">
+       <h4 class="font-bold pb-3">${donateAmount} Taka is Donated ${donateTitle}</h4>
+       <span class='text-gray-500'>Date: ${new Date()}<span>
+    </div>
+    `
+    // document.getElementById('history-section').appendChild(div);
+    historySection.insertBefore(div, historySection.firstChild);
+})
+
+
+// const myBl = parseFloat(document.getElementById('account-balance1').innerText);
+// console.log(typeof myBl)
--- js/utilites.js
+++ js/utilites.js
@@ -2,3 +2,14 @@ function allBalance (id){
     const inputAvailableBalance = parseFloat(document.getElementById(id).innerText);
     return inputAvailableBalance;
 }
+
+function getInnerText (id){
+    const inputBalance = document.getElementById(id).innerText;
+    return inputBalance;
+}
+
+function getInnerValue (id){
+    const innerValue = parseFloat(document.getElementById(id).value)
+    return innerValue;
+}
+// parseFloat(document.getElementById('donate-amount').value)
